f2d32987d7ab6f57ca39c53e300673b65bb66413
jack <lingjiejiang@foxmail.com>
2022-04-10 10:16:40 +0800

    last

--- 第二讲-数据结构/tempCodeRunnerFile.cpp
+++ 第二讲-数据结构/tempCodeRunnerFile.cpp
@@ -1,42 +1,4 @@
-#include<iostream>
-using namespace std;
-const int N=100010;
-int son[N][26],cnt[N],id=0;
-void insert(string s)
-{
-    int p=0;
-    for(int i=0;i<s.size();i++)
+     for(int j=head;j!=-1;j=ne[j])
     {
-        int t=s[i]-'a';
-        if(!son[p][t]) son[p][t]=++id;
-        p=son[p][t];
+        cout<<e[j]<<" ";
     }
-    cnt[p]++;
-}
-int query(string s)
-{
-    int p=0;
-    for(int i=0;i<s.size();i++)
-    {
-        int t=s[i]-'a';
-        if(!son[p][t]) return 0;
-        p=son[p][t];
-    }
-    return cnt[p];
-}
-int main()
-{
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        char oper;
-        string s;
-        cin>>oper>>s;
-        if(oper=='I') insert(s);
-        else {
-            cout<<query(s);
-        }
-    }
-    return 0;
-}
