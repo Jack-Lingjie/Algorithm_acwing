ae0e1a75f548f149729d513ef0490431b7ac1e92
jack <lingjiejiang@foxmail.com>
2022-04-11 12:32:16 +0800

    表达式和

--- 第二讲-数据结构/op.cpp
+++ 第二讲-数据结构/op.cpp
@@ -5,79 +5,48 @@ using namespace std;
 
 stack<char> op;
 stack<int> num;
-unordered_map<char, int> prior{ {'+',1},{'-',1},{'*',2},{'/',2} };
-int ans;
+int ans=0;
 void eval()
 {
-	int y = num.top();
-	num.pop();
-	int x = num.top();
-	num.pop();
-	// cout<<"x:"<<x<<"y:"<<y<<endl;
-	char operate = op.top();
-	if (operate == '+') num.push(x + y);
-	else if (operate == '-') num.push(x - y);
-	else if (operate == '*') num.push(x * y);
-	else num.push(x / y);
-	op.pop();
-	// cout<<num.top()<<"size:"<<num.size()<<endl;
+	int y=num.top();num.pop();
+	int x=num.top();num.pop();
+	char sign=op.top();op.pop();
+	if(sign=='+') num.push(x+y);
+	else if(sign=='-') num.push(x-y);
+	else if(sign=='*') num.push(x*y);
+	else num.push(x/y);
 }
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
 	string s;
-	cin >> s;
-	for (int i = 0; i < s.size(); i++)
+	cin>>s;
+	unordered_map<char,int> priority{{'+',1},{'-',1},{'*',2},{'/',2}};
+	for(int i=0;i<s.size();i++)
 	{
-		
-		
-		// cout<<s[i]<<endl;
-		// cout<<"i:"<<i<<" "<<num.size()<<endl;
-		if (i<s.size()&&s[i] >= '0' && s[i] <= '9')//数字
+		if(s[i]>='0'&&s[i]<='9')
 		{
-			int t = 0;
-			while (s[i] >= '0' && s[i] <= '9')
+			int sum=0;
+			while(i<s.size()&&s[i]>='0'&&s[i]<='9')
 			{
-				
-				t *= 10;
-				t += s[i] - '0';
+				sum*=10;
+				sum+=s[i]-'0';
 				i++;
 			}
-			num.push(t);
-			// cout<<num.top();
+			num.push(sum);
 			i--;
-			// cout<<t<<endl;
-		}
-
-		else if(s[i]=='(')
+		}else if(s[i]=='(')
 		{
 			op.push(s[i]);
-		}
-		else if (s[i] == ')')
+		}else if(s[i]==')')
 		{
-			
-			
-			while (op.top() != '(')
-			{
-				eval();
-			}
-			op.pop();
-		}
-		else 
-		{
-			
-			while (!op.empty()&&op.top()!='(' && prior[op.top()] >= prior[s[i]]) 
-			{
-				
-				eval();
-			}
+			while(op.top()!='(') eval();
+			op.pop();//弹出（
+		}else{//其他符号
+			while(!op.empty()&&op.top()!='('&&priority[op.top()]>=priority[s[i]]) eval();
 			op.push(s[i]);
 		}
 	}
-	// cout<<"out"<<num.top()<<" size"<< num.size()<<endl;
-	while (!op.empty()) eval();
-	cout << num.top() << endl;
+	while(!op.empty()) eval();
+	cout<<num.top()<<endl;
 	return 0;
-
 }
